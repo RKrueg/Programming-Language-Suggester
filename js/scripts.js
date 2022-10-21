98bbe318cd558eb540b54ed919c25bcf68cba780
RKrueg <thegckid@gmail.com>
2022-10-21 11:48:10 -0700

    BUG still am not able to find why submit will not work, preventDefault is working, removeAttribute class and hidden is there, will try looking for more typos possibly?

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
 function languageSelector(event) {
   event.preventDefault();
 
-  let ruby = document.querySelector('div#ruby');
-  let cSharp = document.querySelector('div#cSharp');
-  let swift = document.querySelector('div#swift');
+  // create variables for results
+  let ruby = document.querySelector("div#ruby");
+  let cSharp = document.querySelector("div#cSharp");
+  let swift = document.querySelector("div#swift");
 
-  let singer = document.querySelector("input[name='singer']:checked").value;
-  let entertainment = document.querySelector("input[name='entertainment']:checked").value;
-  let computer = document.querySelector("input[name='computer']:checked").value;
+   //setting the attribute to the results to hidden
+   ruby.setAttribute("class", "hidden");
+   cSharp.setAttribute('class', "hidden");
+   swift.setAttribute("class", "hidden");
 
-  ruby.setAttribute("class", "hidden");
-  cSharp.setAttribute('class', "hidden");
-  swift.setAttribute("class", "hidden");
+  //value from user input
+  const singer = document.querySelector("input[name='singer']:checked").value;
+  const entertainment = document.querySelector("input[name='entertainment']:checked").value;
+  const computer = document.querySelector("input[name='computer']:checked").value;
 
-  if (singer === 'tayloSwift' && entertainment === 'reading' ) {
+  //if and else if statements...remove attribute to display results
+  if (singer === "tayloSwift" && entertainment === "reading" ) {
     ruby.removeAttribute("class", "hidden");
-  }
-  else if (singer === "tayloSwift" && entertainment === "movies") {
+  } else if (singer === "tayloSwift" && entertainment === "movies") {
     cSharp.removeAttribute("class", "hidden");
   }
 }
