8458aa243a9aedf3be5bed225a91ac4b3951d589
RKrueg <thegckid@gmail.com>
2022-10-21 10:29:29 -0700

    BUG submit button will not work

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 function languages(event) {
   event.preventDefault();
 
-  let ruby = document.querySelector();
-  let cSharp = document.querySelector();
-  let swift = document.querySelector();
+  let ruby = document.querySelector('div#ruby');
+  let cSharp = document.querySelector('div#cSharp');
+  let swift = document.querySelector('div#swift');
 
+  let singer = document.querySelector("input[name='singer']:checked").value;
+  let entertainment = document.querySelector("input[name='entertainment']:checked").value;
+  let computer = document.querySelector("input[name='computer']:checked").value;
+
+  ruby.setAttribute("class", "hidden");
+  cSharp.setAttribute('class', "hidden");
+  swift.setAttribute("class", "hidden");
+
+  if (singer === 'tayloSwift' && entertainment === 'reading' ) {
+    ruby.removeAttribute('class', 'hidden');
+  }
+  else if (singer === "tayloSwift" && entertainment === "movies") {
+    cSharp.removeAttribute("class", "hidden")
+  }
+}
 
 window.addEventListener("load", function () {
-  const form = this.document.getElementById("language");
+  const form = document.getElementById("language");
   form.addEventListener("submit", languages)
-})
-}
+});
