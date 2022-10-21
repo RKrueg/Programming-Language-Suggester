52b3d46ec7fd794784fd0700ac3f8eab142abc45
RKrueg <thegckid@gmail.com>
2022-10-21 09:12:53 -0700

    add event listener load

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function languages(event) {
+  event.preventDefault();
+
+  let ruby = document.querySelector();
+  let cSharp = document.querySelector();
+  let swift = document.querySelector();
+
+
+window.addEventListener("load", function () {
+  const form = this.document.getElementById("language");
+  form.addEventListener("submit", languages)
+})
+}
