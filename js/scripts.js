4aca4bf2279ec28370734304e9f6ae30df05d5fd
RKrueg <thegckid@gmail.com>
2022-10-21 14:26:53 -0700

    make heading text more readable, also fix if and else if statements to work properly

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,16 @@ function languageSelector(event) {
 
   let singer = document.querySelector("input[name='singer']:checked").value;
   let computer = document.querySelector("input[name='computer']:checked").value;
-
+  let entertainment = document.querySelector("input[name='entertainment']:checked").value
   
   if (singer === "taylorSwift" && computer === "mac") {
-    ruby.removeAttribute("class", "hidden");
+    swift.removeAttribute("class", "hidden");
   } else if (singer === "taylorSwift" && computer === "windows") {
-    swift.removeAttribute("class", "hidden")
+    swift.removeAttribute("class", "hidden");
+   } else if (singer === "nirvana" && computer === "windows") {
+    ruby.removeAttribute("class", "hidden");
   } else if (singer === "nirvana" && computer === "mac") {
     cSharp.removeAttribute("class", "hidden");
-  } else if (singer === "nirvana" && computer === "windows") {
-    ruby.removeAttribute("class", "hidden");
   } else if (singer === "eltonJohn" && computer === "mac") {
     cSharp.removeAttribute("class", "hidden");
   } else if (singer === "eltonJohn" && computer === "windows") {
