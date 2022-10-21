b60a577710a7b7ee0d888266ae335a641c5b9742
RKrueg <thegckid@gmail.com>
2022-10-21 12:06:27 -0700

    solve one of my submit problems, discovered the error was in the if and else if statements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,22 @@
 function languageSelector(event) {
   event.preventDefault();
 
-  // create variables for results
   let ruby = document.querySelector("div#ruby");
   let cSharp = document.querySelector("div#cSharp");
   let swift = document.querySelector("div#swift");
 
-   //setting the attribute to the results to hidden
    ruby.setAttribute("class", "hidden");
    cSharp.setAttribute('class', "hidden");
    swift.setAttribute("class", "hidden");
 
-  //value from user input
-  const singer = document.querySelector("input[name='singer']:checked").value;
-  const entertainment = document.querySelector("input[name='entertainment']:checked").value;
-  const computer = document.querySelector("input[name='computer']:checked").value;
+  let singer = document.querySelector("input[name='singer']:checked").value;
+  let entertainment = document.querySelector("input[name='entertainment']:checked").value;
+  let computer = document.querySelector("input[name='computer']:checked").value;
 
-  //if and else if statements...remove attribute to display results
-  if (singer === "tayloSwift" && entertainment === "reading" ) {
+  if (singer === "tayloSwift" && entertainment === "reading") {
     ruby.removeAttribute("class", "hidden");
+  } else if (singer === "taylorSwift" && computer === "mac") {
+    swift.removeAttribute("class", "hidden")
   } else if (singer === "tayloSwift" && entertainment === "movies") {
     cSharp.removeAttribute("class", "hidden");
   }
